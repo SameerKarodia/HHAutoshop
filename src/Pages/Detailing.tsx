b667831984cc24db0c0cb2b503ca745455dc6ac1
Sameer <sameerkarodia@gmail.com>
2025-04-21 21:49:34 -0400

    new detailing cards

--- src/Pages/Detailing.tsx
+++ src/Pages/Detailing.tsx
@@ -6,9 +6,10 @@ import Header from "../Header/Header";
 import interior10 from "../assets/interior10.jpg";
 import interior11 from "../assets/interior11.jpg";
 import interior12 from "../assets/interior12.jpg";
-import exterior11 from "../assets/exterior11.jpg";
-import engine10 from "../assets/engine10.png";
-import Iframe from "../Iframe";
+
+import bronzeCard from "../assets/bronzeCard.png";
+import silverCard from "../assets/silvercard.png";
+import goldCard from "../assets/goldcard.png";
 
 const INTERIOR = [interior10, interior11, interior12];
 <div className={slidestyles.imageSlider}>
@@ -29,58 +30,15 @@ function Detailing() {
 
         <div className={styles.detailingContainer}>
           <div className={styles.bronzeDetailing}>
-            <h2>Bronze Package</h2>
-            <h4>Basic Clean & Shine</h4>
-            <img src={exterior11} alt="exterior" />
-            <h5>Starting from $120</h5>
-            <ul>
-              <li>Exterior Hand Wash & Dry</li>
-              <li>Interior Vacuuming (Seats and Carpets)</li>
-              <li>Air Freshener Application</li>
-            </ul>
-            <Iframe url="https://calendly.com/hhautoshop04/detailing" />
-
-            {/* <button className={styles.bookButton}>Book Now</button> */}
+            <img src={bronzeCard} />
           </div>
+
           <div className={styles.silverDetailing}>
-            <h2>Silver Package</h2>
-            <h4>Deep Clean & Protect</h4>
-            <div className={slidestyles.imageSlider}>
-              <ImageSlider imageUrls={INTERIOR} />
-            </div>
-            <h5>Starting from $230</h5>
-            <ul>
-              <li>
-                Interior Dusting & Wipe down (Door Jams, Dashboard, Console)
-              </li>
-              <li>Interior Deep Vacuuming (Including Trunk) </li>
-              <li>Wheels, Tires, and Wheel Wells Deep Cleaned</li>
-              <li>Tire Shine Application </li>
-              (Includes Bronze Package Services)
-            </ul>
-            <Iframe url="https://calendly.com/hhautoshop04/detailing" />
-
-            {/* <button className={styles.bookButton}>Book Now</button> */}
+            <img src={silverCard} />
           </div>
+
           <div className={styles.goldDetailing}>
-            <h2>Gold Package</h2>
-            <h4>Full Detail and Restoration</h4>
-            <img src={engine10} alt="engine" />
-            <h5>Starting from $350</h5>
-            <ul>
-              <li>Streak-Free Window Cleaning (Inside & Out)</li>
-              <li>Headlight & Taillight Polishing</li>
-              <li>Full Interior Shampoo</li>
-              <li>Steam Cleaning</li>
-              <li>Bug, Tar, and Road Grime Removal</li>
-              <li>Paint Correction</li>
-              <li>Clay Bar Treatment for Paint Decontamination</li>
-              <li>Engine Bay Cleaning </li>(Silver Package Services Included)
-            </ul>
-            {/* <div className={styles.bookButtontest}>
-              <Iframe />
-            </div> */}
-            <Iframe url="https://calendly.com/hhautoshop04/detailing" />
+            <img src={goldCard} />
           </div>
         </div>
       </div>
